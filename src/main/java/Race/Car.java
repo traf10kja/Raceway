9c8ce1280e170eb8104e667f45d0811212869b14
Alexey <traf10@bk.ru>
2022-02-26 15:18:08 +0700

    Home Work 5

--- src/main/java/Race/Car.java
+++ src/main/java/Race/Car.java
@@ -1,6 +1,12 @@
 package Race;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.CyclicBarrier;
+
 public class Car implements Runnable{
+
+    private CyclicBarrier barrier;
+    private ArrayBlockingQueue<Car> finished;
     private static int CARS_COUNT;
     static {
         CARS_COUNT = 0;
@@ -14,11 +20,14 @@ public class Car implements Runnable{
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CyclicBarrier barrier, ArrayBlockingQueue<Car> finished) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.barrier = barrier;
+        this.finished = finished;
+
     }
     @Override
     public void run() {
@@ -26,11 +35,18 @@ public class Car implements Runnable{
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            barrier.await();
+            barrier.await();
+
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
+            }
+
+
+            finished.put(this);
+            barrier.await();
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
         }
     }
 }
