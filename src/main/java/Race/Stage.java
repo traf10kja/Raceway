65657a2163237b7c2cc571aba71ccdeec9baa829
Alexey <traf10@bk.ru>
2022-02-26 12:27:22 +0700

    HW_5

---
+++ src/main/java/Race/Stage.java
@@ -0,0 +1,10 @@
+package Race;
+
+public abstract class Stage {
+    protected int length;
+    protected String description;
+    public String getDescription() {
+        return description;
+    }
+    public abstract void go(Car c);
+}
